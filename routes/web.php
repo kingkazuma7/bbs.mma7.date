64d87f69840538ec9486f39c3c0c26cbb4437d21
kingkazuma7 <your.nasionasio7@gmail.com>
2025-10-17 15:42:19 +0900

    feat: Define resource routes for Event, Thread, and Post, and add Fighter threads route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\FighterController;
+use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Eventリソースルート
+Route::resource('events', EventController::class);
+
+// Fighterに関連するThread
+Route::get('fighters/{fighter}/threads', [FighterController::class, 'threads'])->name('fighters.threads');
+
+// Thread詳細とそれに紐付くPost
+Route::resource('threads', ThreadController::class);
+Route::resource('threads.posts', PostController::class)->only(['store']);
