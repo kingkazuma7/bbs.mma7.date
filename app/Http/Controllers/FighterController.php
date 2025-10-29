d731df2ed92fe7358a65c7d1f9d742abdc2be3fa
kingkazuma7 <your.nasionasio7@gmail.com>
2025-10-29 14:55:12 +0900

    feat: Rename threads method to show in FighterController and add threads view

--- app/Http/Controllers/FighterController.php
+++ app/Http/Controllers/FighterController.php
@@ -13,8 +13,13 @@ class FighterController extends Controller
         return view('fighters.index', compact('fighters'));
     }
 
-    public function threads(Fighter $fighter)
+    public function show(Fighter $fighter)
     {
         return view('fighters.show', compact('fighter'));
+    }
+
+    public function threads(Fighter $fighter)
+    {
+        return view('fighters.threads', compact('fighter'));
     }
 }
--- resources/views/fighters/show.blade.php
+++ resources/views/fighters/show.blade.php
@@ -2,20 +2,11 @@
 
 @section('content')
 <div class="container">
-    <h1>{{ $fighter->name }} のスレッド</h1>
-    <p>ファイターID: {{ $fighter->id }}</p>
-
-    <h2 class="mt-4">関連スレッド一覧</h2>
-    @if($fighter->threads->isEmpty())
-        <p>このファイターに関連するスレッドはありません。</p>
-    @else
-        <ul class="list-group">
-            @foreach($fighter->threads as $thread)
-                <li class="list-group-item">
-                    <a href="{{ route('threads.show', $thread) }}">{{ $thread->title }}</a>
-                </li>
-            @endforeach
-        </ul>
-    @endif
+    <h1>Fighter Details: {{ $fighter->name }}</h1>
+    <p>Fighter ID: {{ $fighter->id }}</p>
+    <p>Created At: {{ $fighter->created_at }}</p>
+    <p>Updated At: {{ $fighter->updated_at }}</p>
+    <a href="{{ route('fighters.index') }}" class="btn btn-primary">Back to Fighters</a>
+    <a href="{{ route('fighters.threads', $fighter) }}" class="btn btn-info">View Threads</a>
 </div>
 @endsection
