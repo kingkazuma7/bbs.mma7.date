10ef09244a93c99f7ec6c3ecb878de074a8f927a
kingkazuma7 <your.nasionasio7@gmail.com>
2025-11-17 15:06:24 +0900

    refactor: Update Vite configuration and restructure JavaScript files for improved organization and functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/js/app.js', 'resources/js/thread_show.js'])
+    <script src="{{ asset('js/thread_show.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body class="d-flex flex-column min-vh-100">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
