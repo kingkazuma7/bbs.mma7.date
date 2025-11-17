15f2c66fa45ba6845b652718885f20e9dbb3a77b
kingkazuma7 <your.nasionasio7@gmail.com>
2025-11-17 14:51:06 +0900

    refactor: Remove foreign key constraint on fighter_id in threads migration to simplify schema

--- database/migrations/2025_10_16_120543_create_threads_table.php
+++ database/migrations/2025_10_16_120543_create_threads_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('fighter_id')->nullable()->constrained()->onDelete('set null');
             $table->string('title');
             $table->timestamps();
         });
