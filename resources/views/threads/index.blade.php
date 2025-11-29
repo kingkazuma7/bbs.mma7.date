562de2c53d47a86624a0f7aa93d0386d02f4b125
kingkazuma7 <your.nasionasio7@gmail.com>
2025-11-29 11:38:12 +0900

    feat: Add usage instructions for creating and viewing threads in Japanese to the threads index page

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="card mb-3">
+    <div class="card-header">使い方</div>
+    <div class="card-body">
+        <ul class="list-unstyled">
+            <li>新しいスレッドを作成するには「新規スレッド作成」ボタンをクリックしてください。</li>
+            <li>既存のスレッドを見るには、スレッドタイトルをクリックしてください。</li>
+            <li>スレッド内で返信するには、スレッド詳細ページで投稿フォームを使用してください。</li>
+            <li>完全匿名で投稿できます。</li>
+        </ul>
+    </div>
+</div>
 <div class="container">
     <h1>{{ __('messages.threads_list') }}</h1>
     <a href="{{ route('threads.create') }}" class="btn btn-primary mb-3">{{ __('messages.create_new_thread') }}</a>
