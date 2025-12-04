50ed47f4d3cb17b7e0208eda55034d9e877b5cf9
kingkazuma7 <your.nasionasio7@gmail.com>
2025-12-04 12:10:32 +0900

    fix: Conditionally load SCSS styles with Vite based on the environment to prevent issues in production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/scss/app.scss'])
+    @if(App::environment() !== 'production')
+        @vite('resources/scss/app.scss')
+    @endif
     <script src="{{ asset('js/main.js') }}" defer></script>
 </head>
 <body class="d-flex flex-column min-vh-100">
