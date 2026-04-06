ab9ce332a099a7920d1a05a267d07d1cef9ff450
kingkazuma7 <your.nasionasio7@gmail.com>
2026-04-07 08:16:34 +0900

    refactor: Remove Post and Thread controllers, models, migrations, and related views to streamline the application structure

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ThreadController;
-use App\Http\Controllers\PostController;
-
-Route::redirect('/', '/threads');
-
 // Eventリソースルート
 //    Route::resource('events', EventController::class);
 
 // Fighterリソースルート
-
-// Thread詳細とそれに紐付くPost
-Route::resource('threads', ThreadController::class);
-Route::resource('threads.posts', PostController::class)->only(['store']);
