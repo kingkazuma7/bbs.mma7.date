7217fb05dbe51dee89d18995e82d938abb3cf744
kingkazuma7 <your.nasionasio7@gmail.com>
2026-04-26 10:22:56 +0900

    feat: Googleサイトの検証メタタグを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="6SNLD4Z1k89AAcyohUG5cpxsqiCxqkX12VetSTQVdIo" />
 
     @if(isset($seoModel))
         {!! seo()->for($seoModel) !!}
