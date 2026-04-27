82005a14c098946e0bda70d118ebe9bd15bc6958
kingkazuma7 <your.nasionasio7@gmail.com>
2026-04-27 16:38:46 +0900

    feat: add favicon to application layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="google-site-verification" content="6SNLD4Z1k89AAcyohUG5cpxsqiCxqkX12VetSTQVdIo" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-5VMVNQVCFB"></script>
